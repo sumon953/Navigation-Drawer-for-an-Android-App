8cb2de1edea4dff155b1499b5c4f3d000fd40965
sumonsorder953@gmail.com <sumonsorder953@gmail.com>
2018-11-06 17:34:55 +0600

    64

---
+++ NavigationDrawer/app/src/main/java/com/shoedazzle/m/navigationdrawer/MainActivity.java
@@ -0,0 +1,36 @@
+package com.shoedazzle.m.navigationdrawer;
+
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    DrawerLayout mdrawerlayout;
+    ActionBarDrawerToggle mToggle;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mdrawerlayout = findViewById(R.id.drawer);
+
+        mToggle = new ActionBarDrawerToggle(this,mdrawerlayout,R.string.open,R.string.close);
+        mdrawerlayout.addDrawerListener(mToggle);
+        mToggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if (mToggle.onOptionsItemSelected(item)){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
